408b41960498430ec3957b33c7c667a84b3aebd7
Cherrykill <nvietanh093@gmail.com>
2026-07-17 11:46:56 +0700

    feat: connect to Supabase and fetch products from database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 // ANCHOR: THEM CAC DEPENDENCIES CAN THIET
-const express = require("express");
+import "dotenv/config";
+import express from "express";
+import { createClient } from "@supabase/supabase-js";
+
+// ANCHOR: DINH NGHIA SERVER
 const app = express();
 const port = 3000;
+
+// ANCHOR: KET NOI SUPABASE
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 // ANCHOR: DINH NGHIA DUONG DAN
 // Route gốc: http://localhost:3000/
@@ -9,24 +18,17 @@ app.get("/", (req, res) => {
   res.send("Chao mung den vs web E-commerce");
 });
 // Route /products: http://localhost:3000/products
-app.get("/products", (req, res) => {
-  res.json([
-    { id: 1, name: "Áo thun nam Basic", price: 199000 },
-    { id: 2, name: "Áo sơ mi trắng", price: 349000 },
-    { id: 3, name: "Quần jean xanh Slim Fit", price: 499000 },
-    { id: 4, name: "Quần jean xanh Slim Fit", price: 499000 },
-    { id: 5, name: "Quần jean xanh Slim Fit", price: 499000 },
-    {
-      id: 4,
-      name: "Quần kaki đen",
-      price: 429000,
-    },
-    {
-      id: 5,
-      name: "Áo hoodie Unisex",
-      price: 599000,
-    },
-  ]);
+app.get("/products", async (req, res) => {
+  try {
+    // Supabase se tra ve mot obj co 2 key la { data: [...], error: null }
+    const { data, error } = await supabase.from("products").select("*");
+
+    // Neu obj dc tra ve co key error k phai la null thi truyen luon gtri loi vao thang tham so error o catch(error)
+    if (error) throw error;
+    res.json(data); // Tra ra data binh thuong neu k co loi
+  } catch (error) { // O day "error" giong nhu mot doi tuong co 3 thuoc tinh. VD: error.message la thong diep loi, error.stack la vi tri loi, error.name la ten loi
+    res.status(500).json({ error: error.message });
+  }
 });
 // Route /about: http://localhost:3000/about
 app.get("/about", (req, res) => {
